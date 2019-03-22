d6edfdf043c13fcae9977d491a61d891c80df55a
root <root@ip-172-31-33-5.us-west-2.compute.internal>
2019-03-22 08:32:39 +0000

    Added process id and child id and hostname in return

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import os
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World %d %d %s"%(os.getppid(),os.getpid(),os.uname()[1])
 
 if __name__ == "__main__":
     application.run()
